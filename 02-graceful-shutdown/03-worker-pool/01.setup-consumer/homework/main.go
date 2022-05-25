4260634c9c2598e5b0daf6afba522e2a4b4a3730
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-05-25 12:09:09 +0800

    docs(03.consumer): add description
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- 02-graceful-shutdown/03-worker-pool/01.setup-consumer/homework/main.go
+++ 02-graceful-shutdown/03-worker-pool/01.setup-consumer/homework/main.go
@@ -8,6 +8,7 @@ func main() {
 	go q.startConsumer()
 
 	for i := 0; i < 10; i++ {
+		// change the parameter for add task and print the value of variable.
 		if err := q.addTask(i); err != nil {
 			log.Println(err)
 		}
